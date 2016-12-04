554684cb82cd7fd4436f845b79603e7a443e3360
Yelena Gindin <yelenar@gmail.com>
2016-12-04 15:56:07 +0200

    adding block moving via css3 transform

--- group/app.js
+++ group/app.js
@@ -2,32 +2,37 @@ window.onload = function() {
 
     var bcontainer = document.getElementById("bcontainer");
     var posy = 0;
+    var posx = 0;
+    var rotdeg = 0;
     document.onkeydown = function(evt){
-	console.log(evt);
+//	console.log(evt);
 	switch (evt.key) {
 	case "ArrowDown":
 	    posy += 10;
-	    bcontainer.style.top = posy + 'px';
-	    
+	    //via top/left changing
+	    //bcontainer.style.top = posy + 'px';
+	    //via css3 transform
+	    //bcontainer.style.transform = "translate(" + posx + "px, " + posy + "px)";	    
 	    break;
-/*	case "ArrowUp":
-	    stopCurrentAnimation();
-	    moveUp(evt);
+	case "ArrowUp":
+	    posy -= 10;
+	    //bcontainer.style.transform = "translate(" + posx + "px, " + posy + "px)";
 	    break;
 	case "ArrowLeft":
-	    stopCurrentAnimation();
-	    moveLeft(evt);
+	    posx -= 10;
+	    //bcontainer.style.transform = "translate(" + posx + "px, " + posy + "px)";
 	    break;
 	case "ArrowRight":
-	    stopCurrentAnimation();
-	    moveRight(evt);
+	    posx += 10;
+	    //bcontainer.style.transform = "translate(" + posx + "px, "  + posy + "px)";
 	    break;
-	case "=":
-	    stopCurrentAnimation();
-	    break;*/
+	case "r":
+	    rotdeg += 3;
+	    break;
 	default:
 	    break;
 	}
+	bcontainer.style.transform = "rotate(" + rotdeg + "deg) " +  "translate(" + posx + "px, "  + posy + "px)"
 	
     }
 }

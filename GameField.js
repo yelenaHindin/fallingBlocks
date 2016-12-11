f84b6013da68167e5cb3db6bcd536a16e910569c
Yelena Gindin <yelenar@gmail.com>
2016-12-11 15:32:06 +0200

    isValid

--- GameField.js
+++ GameField.js
@@ -1,16 +1,23 @@
 GameField = function(h, w){
     this.fieldHeight = h;
     this.fieldWidth = w;
+    this.field = [][];
 }
 
 
 GameField.prototype.isValid = function(coord) {
-    
+    if (GameField.isCoord(coord)){
+	return (((coord[0] < this.fieldWidth) && (coord[1] < this.fieldHeight) && this.field[coord[0][coord[1]] != null));
+    } else if (Array.isArray(coord) && coord.every((e) => GameField.isCoord(e))) {
+	coord.forEach((e) => isValid(e));
+    } else {
+	throw new Error("Unexpected argument type " + coord);
+    }
 
 }
 
 GameField.isCoord = function(a) {
-    return Array.isArray(a) && !isNan(a[0]) && !isNaN(a[1]) && a.length = 2;
+    return Array.isArray(a) && !isNaN(a[0]) && !isNaN(a[1]) && a.length = 2;
 }
 
 GameField.prototype.set = function(coord, value) {
